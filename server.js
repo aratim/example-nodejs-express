2214f93bb1a32d654650347bbe9cd5b55b1e9b53
arati.mahimane <arati.mahimane@rackspace.com>
2014-05-05 14:01:51 -0500

    changed server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.get('/', function(req, res){
   var message = process.env.HELLO;
   if (typeof(message) == "undefined") {
-  	message = "after git push!  "
+  	message = "Arts!  "
   }
   res.send('Hello ' + message);
 });
